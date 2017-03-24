8b065aa751936507334f4ac5bd1b678c81fc3f4b
Alves de Jesus Marvin <pocchami@gmail.com>
2017-03-24 20:58:08 +0100

    Update User

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -2,6 +2,7 @@
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake\Auth\DefaultPasswordHasher;
 
 /**
  * User Entity
@@ -31,4 +32,9 @@ class User extends Entity
         '*' => true,
         'USER_ID' => false
     ];
+
+    protected function _setUSER_PASSWORD($password)
+    {
+        return (new DefaultPasswordHasher)->hash($password);
+    }
 }
