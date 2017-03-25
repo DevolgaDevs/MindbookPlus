cf22728ec04f536f50abd7949b414eda10348b0e
Alves de Jesus Marvin <pocchami@gmail.com>
2017-03-25 12:10:17 +0100

    Update register method

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -38,14 +38,21 @@ class UsersController extends AppController
 
     public function register()
     {
-        $user = $this->Users->newEntity($this->request->getData());
-        if ($this->Users->save($user)) {
-            $this->Auth->setUser($user->toArray());
-            return $this->redirect([
-            'controller' => 'Users',
-            'action' => 'home'
-            ]);
+        $user = $this->Users->newEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+
+                return $this->redirect([
+                'controller' => 'Users',
+                'action' => 'index'
+                            ]);
+            }
+            $this->Flash->error(__('The user could not be saved. Please, try again.'));
         }
+        $this->set(compact('user'));
+        $this->set('_serialize', ['user']);
     }
 
     /**
